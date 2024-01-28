a4b7120c46f3e8a54a22de4e2b0144eb65e8231f
zehraina <inazehra6@gmail.com>
2024-01-28 18:42:08 +0530

    Created the drop-down

--- CivicEye/App.js
+++ CivicEye/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, TouchableOpacity, useState } from 'react-native';
+import { SelectCountry } from 'react-native-element-dropdown';
+// import React, { useState } from 'react';
+//   import { StyleSheet } from 'react-native';
+
+// import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import Home from "./app/screens/Home";
 
 export default function App() {
@@ -9,5 +14,48 @@ export default function App() {
 
   );
 }
+
+
+
+// export default function App() {
+//   const [selectImage, setselectImage] = useState('')
+//   const ImagePicker = () => {
+//     let options = {
+//       storageOptions: {
+//         path: "image"
+//       },
+//     };
+
+//     launchImageLibrary(options, response => {
+//       setselectImage(response.assets[0].uri);
+//       console.log(response.assets[0].uri);
+//     });
+//   };
+//   return (
+//     <SafeAreaView style={{ flex: 1 }}>
+//       <View style={{ height: 400, width: "100%" }}>
+//         <Image style={{ height: 400, width: "100%" }}
+//           source={{uri:selectImage}}
+//         />
+//         <TouchableOpacity
+//           onPress={() => {
+//             ImagePicker();
+//           }}
+//           style={{
+//             marginTop: 20,
+//             height: 50,
+//             width: "60%",
+//             borderRadius: 20,
+//             backgroundColor: 'skyblue',
+//           justifyContent:"center",
+//           alignItems:"center",
+//           slignSelf:"center",
+//           }}>
+//           <Text style={{fontSize:20}}>Gallery</Text>
+//         </TouchableOpacity>
+//       </View>
+//        </SafeAreaView>
+//   );
+// }
 
 
--- CivicEye/app/screens/Home.js
+++ CivicEye/app/screens/Home.js
@@ -1,65 +1,112 @@
+
+
 import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Button, TextInput, Image, Alert } from 'react-native';
 import * as ImagePicker from "expo-image-picker";
+import { SelectCountry } from 'react-native-element-dropdown';
 
-
+const local_data = [
+    {
+    value: '0',  // Change value to '0' for the placeholder
+    label: 'Select category',
+  },
+  {
+    value: '1',
+    label: 'Garbage',
+    image: {
+      uri: 'https://www.vigcenter.com/public/all/images/default-image.jpg',
+    },
+  },
+  {
+    value: '2',
+    label: 'Infrastructure',
+    image: {
+      uri: 'https://www.vigcenter.com/public/all/images/default-image.jpg',
+    },
+  },
+  {
+    value: '3',
+    label: 'Animals',
+    image: {
+      uri: 'https://www.vigcenter.com/public/all/images/default-image.jpg',
+    },
+  },
+  {
+    value: '4',
+    label: 'Something',
+    image: {
+      uri: 'https://www.vigcenter.com/public/all/images/default-image.jpg',
+    },
+  },
+];
 
 function Home() {
-  const [isClicked, setIsClicked] = useState("false");
-   const [file, setFile] = useState(null);
-  
-    // Stores any error message
-    const [error, setError] = useState(null);
-  
-    // Function to pick an image from
-    //the device's media library
-    const pickImage = async () => {
-        const { status } = await ImagePicker.
-            requestMediaLibraryPermissionsAsync();
-  
-        if (status !== "granted") {
-  
-            // If permission is denied, show an alert
-            Alert.alert(
-                "Permission Denied",
-                `Sorry, we need camera
-                 roll permission to upload images.`
-            );
-        } else {
-  
-            // Launch the image library and get
-            // the selected image
-            const result =
-                await ImagePicker.launchImageLibraryAsync();
-  
-            if (!result.cancelled) {
-  
-                // If an image is selected (not cancelled),
-                // update the file state variable
-                setFile(result.uri);
-  
-                // Clear any previous errors
-                setError(null);
-            }
-        }
-    };
-  
-  const open_description = () => {
-    setIsClicked(!isClicked);
-   
+  const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+   const [isClicked, setIsClicked] = useState(false);
+  const [file, setFile] = useState(null);
 
-};
-  
-  
+  // Stores any error message
+  const [error, setError] = useState(null);
+
+  // Function to pick an image from
+  // the device's media library
+  const pickImage = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (status !== "granted") {
+      // If permission is denied, show an alert
+      Alert.alert(
+        "Permission Denied",
+        "Sorry, we need camera roll permission to upload images."
+      );
+    } else {
+      // Launch the image library and get
+      // the selected image
+      const result = await ImagePicker.launchImageLibraryAsync();
+
+      if (!result.canceled) {
+        // If an image is selected (not cancelled),
+        // update the file state variable
+        setFile(result.uri);
+
+        // Clear any previous errors
+        setError(null);
+      }
+    }
+  };
+
+  const openDescription = () => {
+    // Handle description opening here
+    setIsClicked(!isClicked);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.heading}>Submit a new concern</Text>
-      <Button
-        style={{ width: "100%" }}
-        color="orange"
-        title="Select category"
-      />
+
+      <View style={styles.dropdownContainer}>
+        {/* <Text style={styles.selectCategoryText}>Select category</Text> */}
+        <SelectCountry
+          style={styles.dropdown}
+          selectedTextStyle={styles.selectedTextStyle}
+          placeholderStyle={styles.placeholderStyle}
+          imageStyle={styles.imageStyle}
+          iconStyle={styles.iconStyle}
+          maxHeight={200}
+          value={'1'}
+          data={local_data}
+          valueField="value"
+          labelField="label"
+          imageField="image"
+          placeholder="Select category"
+          searchPlaceholder="Search..."
+          onChange={() => {}}
+          onOpen={() => setIsDropdownVisible(true)}
+          onClose={() => setIsDropdownVisible(false)}
+        />
+      </View>
+
       <Button
         style={{ width: "100%" }}
         color="orange"
@@ -70,29 +117,30 @@ function Home() {
         style={{ width: "100%" }}
         color="orange"
         title="Description"
-        onPress={()=>open_description()}
+        onPress={openDescription}
       />
-      
-      {isClicked ? <View style={styles.description_popup}>
-        <TextInput
-        placeholder="Write the description here."
-        style={styles.description_input}
-    
-      />
-      </View>:null}
-        
+      {isClicked ? (
+        <View style={styles.description_popup}>
+          <TextInput
+            placeholder="Write the description here."
+            style={styles.description_input}
+          />
+        </View>
+      ) : null}
+      {isDropdownVisible && (
+        <View style={styles.dropdownOverlay} onTouchEnd={() => setIsDropdownVisible(false)} />
+      )}
 
       {file ? (
-                // Display the selected image
-                <View style={styles.imageContainer}>
-                    <Image source={{ uri: file }}
-                        style={styles.image} />
-                </View>
-            ) : (
-                // Display an error message if there's
-                // an error or no image selected
-                <Text style={styles.errorText}>{error}</Text>
-            )}
+        // Display the selected image
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: file }} style={styles.image} />
+        </View>
+      ) : (
+        // Display an error message if there's
+        // an error or no image selected
+        <Text style={styles.errorText}>{error}</Text>
+      )}
     </SafeAreaView>
   );
 }
@@ -101,41 +149,82 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'pink',
-    // justifyContent: 'center',
-    // alignItems: 'center',
     paddingTop: 70,
   },
-
   heading: {
     fontWeight: "bold",
-    fontSize: 20, // Adjust the font size as needed
-    textAlign: 'center', // Center the text
+    fontSize: 26,
+    textAlign: 'center',
   },
-  description_popup: {
+  dropdownContainer: {
+    flexDirection: 'row',
+    // alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: 'orange',
+    padding: 10,
+    // margin: 10,
+    borderRadius: 5,
+    // color:"orange"
+  },
+  selectCategoryText: {
+    // color: "#ffff",
+    textAlign: "center",
+     fontWeight: "bold",
+  },
+  dropdown: {
+    flex: 1,
+    // height: 50,
+    backgroundColor: 'orange',
+    borderRadius: 90,
+    paddingHorizontal: 8,
+  },
+  // imageContainer: {
+  //   height: 100,
+  //   width: 100,
+  // },
+  // image: {
+  //   width: 200,
+  //   height: 200,
+  //   borderRadius: 8,
+  // },
+  dropdownOverlay: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  imageStyle: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+  },
+ placeholderStyle: {
+    fontSize: 100,
+    color: "#fff",
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+    color: "#000",  // Set selected text color to black
+  },
+  
+  // iconStyle: {
+  //   width: 20,
+  //   height: 20,
+  // },
+
+    description_popup: {
     width: "50%",
     backgroundColor: "#fff",
     height: 30,
     alignSelf:"center",
   },
-
   description_input: {
     height: 20,
-    // borderRadius:10,
-    // borderWidth: 0.5,
     borderColor: "#8e8e8e",
     alignSelf: "center",
-    // padd
   },
-
-   imageContainer: {
-        
-  },
-    image: {
-        width: 200,
-        height: 200,
-        borderRadius: 8,
-    },
 });
 
 export default Home;
-
