63609f72b6c75e577ca93773d3eb60bffb113d41
fansan6190 <fanin.s.pbl@gmail.com>
2024-02-18 20:32:10 +0530

    getting all user inputs, converting them to json

--- CivicEye/app/src/main/java/com/example/civiceye/MainActivity.kt
+++ CivicEye/app/src/main/java/com/example/civiceye/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.example.civiceye
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.civiceye.databinding.ActivityMainBinding
+import com.example.civiceye.ui.home.HomeFragment
+
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
--- CivicEye/app/src/main/java/com/example/civiceye/ui/home/HomeFragment.kt
+++ CivicEye/app/src/main/java/com/example/civiceye/ui/home/HomeFragment.kt
@@ -1,29 +1,65 @@
 package com.example.civiceye.ui.home
 
+import android.Manifest
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.SeekBar
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.civiceye.R
 import com.example.civiceye.databinding.FragmentHomeBinding
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
 
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    val categoryToSubcategories = mapOf(
+        "Category 1" to listOf("Subcategory 1.1", "Subcategory 1.2", "Subcategory 1.3"),
+        "Category 2" to listOf("Subcategory 2.1", "Subcategory 2.2", "Subcategory 2.3"),
+        "Category 3" to listOf("Subcategory 3.1", "Subcategory 3.2", "Subcategory 3.3")
+    )
+
+    companion object {
+        const val REQUEST_IMAGE_CAPTURE = 1
+        const val PERMISSION_REQUEST_CODE = 100
+    }
+
+    private lateinit var imageView: ImageView
+    private lateinit var cameraButton: Button
+
+    private val takePictureResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val imageBitmap = result.data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private lateinit var seekBar: SeekBar
+    private lateinit var seekBarValue: TextView
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -41,46 +77,117 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
 
+        val subcategoryAutocomplete: AutoCompleteTextView = binding.autoCompleteTextView
         val spinner1: Spinner = binding.spinner1
-        ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.dropdown_options,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner1.adapter = adapter
+        //val categories = arguments?.getStringArray("categories")?.toList() ?: listOf()
+        val categories = categoryToSubcategories.keys.toList()
+        spinner1.setSelection(0)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner1.adapter = adapter
+        spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val selectedCategory = parent.getItemAtPosition(position).toString()
+
+                // Update subcategories when a category is selected
+                val subcategories = categoryToSubcategories[selectedCategory] ?: listOf()
+                val subadapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, subcategories)
+                subcategoryAutocomplete.setAdapter(subadapter)
+                subcategoryAutocomplete.isEnabled = true
+
+                // Clear inputs on change in category
+                subcategoryAutocomplete.text.clear()
+                seekBar.progress = 0
+                imageView.setImageDrawable(null)
+                //this@HomeFragment.subcategories.clear()
+                //this@HomeFragment.subcategories.addAll(subcategories)
+                subadapter.notifyDataSetChanged()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
         }
 
-        
-        // Initialize the subcategories list with the values from strings.xml
-        val subcategories = resources.getStringArray(R.array.subcategory_dropdown_options).toMutableList()
 
-        val subcategoryAutocomplete: AutoCompleteTextView = binding.autoCompleteTextView
-    
-        // Create the ArrayAdapter with the subcategories list
-        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, subcategories)
-        subcategoryAutocomplete.setAdapter(adapter)
+        val subcategories = arguments?.getStringArray("subcategories")?.toMutableList() ?: mutableListOf()
+        val subadapter = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, subcategories)
+        subcategoryAutocomplete.setAdapter(subadapter)
 
-        // Get the submit button
+
+        //image capture
+        imageView = binding.imageView
+        cameraButton = binding.cameraButton
+
+        cameraButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.CAMERA),
+                    PERMISSION_REQUEST_CODE
+                )
+            } else {
+                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                takePictureResult.launch(takePictureIntent)
+            }
+        }
+
+        //slider
+        seekBar = binding.seekBar
+        seekBarValue = binding.seekBarValue
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                // Update the TextView with the current SeekBar value
+                seekBarValue.text = "$progress"
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Called when the user starts moving the SeekBar.
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Called when the user stops moving the SeekBar.
+            }
+        })
+
         val submitButton: Button = binding.submitButton
-
-        // Set a click listener on the submit button
-                submitButton.setOnClickListener {
+        submitButton.setOnClickListener {
             val newSubcategory = subcategoryAutocomplete.text.toString()
             if (newSubcategory.isNotEmpty() && !subcategories.contains(newSubcategory)) {
-                // Add the new subcategory to the list and update the adapter
                 subcategories.add(newSubcategory)
                 Log.d("HomeFragment", "Subcategories after adding new one: $subcategories")
-                adapter.notifyDataSetChanged()
+                subadapter.notifyDataSetChanged()
                 Log.d("HomeFragment", "Adapter count after notifyDataSetChanged: ${adapter.count}")
                 
-                // Show a toast message
                 Toast.makeText(requireContext(), "New subcategory added: $newSubcategory", Toast.LENGTH_SHORT).show()
             }
+
+            //image to base64
+            val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+            val byteArray = byteArrayOutputStream.toByteArray()
+            val base64Image = Base64.encodeToString(byteArray, Base64.DEFAULT)
+
+            /*
+            can be converted back to normal image in java using-
+            <
+                String base64Image = "...";
+                byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
+                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            >
+            */
+
+            //JSON Data
+            val data = JSONObject().apply {
+                put("category", spinner1.selectedItem)
+                put("subcategory", newSubcategory)
+                put("seekBarValue", seekBar.progress)
+                put("image", base64Image) 
+            }
+            Log.d("HomeFragment", "Data: $data")
         }
         return root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
